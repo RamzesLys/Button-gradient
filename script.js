66c75f653414b8ae6e0075f9704f5c25f8edf112
RamzesLys <ramzes311982@gmail.com>
2023-03-31 15:40:26 +0300

    Wp +

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ myBtn.onmouseout = () => {
 }
 
 let links = document.querySelectorAll('.services');
-// links.forEach((elem) => {
-//   elem.setAttribute("href", "https://www.facebook.com/");
-//   console.log(elem);
-// })
 
-links[0].setAttribute("href", "https://www.facebook.com/");
-links[1].setAttribute("href", "https://www.youtube.com/");
+links[0].setAttribute("href", "http://ipsum.in.ua/services/elektrifikatsiya/");
+links[1].setAttribute("href", "http://ipsum.in.ua/services/sistemy-bezopasnosti/");
+links[2].setAttribute("href", "http://ipsum.in.ua/services/sistemy-kontrolya-dostupa/");
+links[3].setAttribute("href", "http://ipsum.in.ua/services/video-nablyudenie/");
+links[4].setAttribute("href", "http://ipsum.in.ua/services/domofony/");
+links[5].setAttribute("href", "http://ipsum.in.ua/services/internet-i-wi-fi/");
+links[6].setAttribute("href", "http://ipsum.in.ua/services/tv/");
+links[7].setAttribute("href", "http://ipsum.in.ua/services/umnyj-dom/");
 
 console.log(links);
